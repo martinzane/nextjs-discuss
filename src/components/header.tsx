6288e025e75b74265307f32f8d55797a87a56fb8
Martin Zane <martin@zaneweb.com>
2024-05-08 10:27:05 +0200

    Add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,73 @@
+import { auth } from "@/auth";
+import {
+  Avatar,
+  Button,
+  Input,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import Link from "next/link";
+import { ReactNode } from "react";
+import * as actions from "@/actions";
+
+export default async function Header() {
+  const session = await auth();
+
+  let authContent: ReactNode;
+  if (session?.user) {
+    authContent = (
+      <Popover placement="left">
+        <PopoverTrigger>
+          <Avatar src={session.user.image || ""} />
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="p-4">
+            <form action={actions.signOut}>
+              <Button type="submit">Sign out</Button>
+            </form>
+          </div>
+        </PopoverContent>
+      </Popover>
+    );
+  } else {
+    authContent = (
+      <>
+        <NavbarItem>
+          <form action={actions.signIn}>
+            <Button type="submit" color="secondary" variant="bordered">
+              Sign in
+            </Button>
+          </form>
+        </NavbarItem>
+        <NavbarItem>
+          <form action={actions.signIn}>
+            <Button type="submit" color="primary" variant="flat">
+              Sign up
+            </Button>
+          </form>
+        </NavbarItem>
+      </>
+    );
+  }
+
+  return (
+    <Navbar className="shadow mb-6">
+      <NavbarBrand>
+        <Link href="/" className="font-bold">
+          Discuss
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <Input />
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">{authContent}</NavbarContent>
+    </Navbar>
+  );
+}
