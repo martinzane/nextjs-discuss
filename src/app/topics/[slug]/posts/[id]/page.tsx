a5e3463dc48a321d3cc9b68c481722e9f52e0b93
Martin Zane <martin@zaneweb.com>
2024-05-09 10:54:27 +0200

    Merge UI components

--- src/app/topics/[slug]/posts/[id]/page.tsx
+++ src/app/topics/[slug]/posts/[id]/page.tsx
@@ -1,3 +1,21 @@
-export default function PostPage() {
-  return <div>Post Page</div>;
+import Link from "next/link";
+import paths from "@/paths";
+
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    id: string;
+  };
+}
+
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, id } = params;
+
+  return (
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topic(slug)}>
+        {"< "}Back to {slug}
+      </Link>
+    </div>
+  );
 }
