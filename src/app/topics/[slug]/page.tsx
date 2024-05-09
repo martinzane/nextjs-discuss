ccbe5e0c61eaffd120e0a5a9d2fe12d4018989f5
Martin Zane <martin@zaneweb.com>
2024-05-09 10:19:06 +0200

    Add Post Create Form

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,22 @@
-export default function TopicPage() {
-  return <div>Topic Page</div>;
+import PostCreateForm from "@/components/posts/create-form";
+
+interface TopicPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function TopicPage({ params }: TopicPageProps) {
+  const { slug } = params;
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+      </div>
+
+      <div>
+        <PostCreateForm />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/posts/create-form.tsx
@@ -0,0 +1,42 @@
+import {
+  Button,
+  Input,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import FormButton from "../common/form-button";
+
+export default function PostCreateForm() {
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create a Post</Button>
+      </PopoverTrigger>
+
+      <PopoverContent>
+        <form action="">
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-ld">Create a Post</h3>
+
+            <Input
+              name="title"
+              label="Title"
+              labelPlacement="outside"
+              placeholder="Title"
+            />
+
+            <Input
+              name="content"
+              label="Content"
+              labelPlacement="outside"
+              placeholder="Content"
+            />
+
+            <FormButton>Save</FormButton>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+}
