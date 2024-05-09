123e051768ce11eab61810472cd54d1290033ba2
Martin Zane <martin@zaneweb.com>
2024-05-09 10:35:03 +0200

    Add validation to Create post form

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,5 +1,52 @@
 "use server";
 
-export async function createPost() {
+import auth from "@/auth";
+import { z } from "zod";
+import type { Topic } from "@prisma/client";
+import db from "@/db";
+import { redirect } from "next/navigation";
+import paths from "@/paths";
+import { revalidatePath } from "next/cache";
+
+const schema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
+
+interface CreatePostFormState {
+  errors: {
+    title?: string[];
+    content?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createPost(
+  formState: CreatePostFormState,
+  formData: FormData
+): Promise<CreatePostFormState> {
+  const result = schema.safeParse({
+    title: formData.get("title"),
+    content: formData.get("content"),
+  });
+
+  console.log(result.success);
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth.session();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to do this."],
+      },
+    };
+  }
+
+  return { errors: {} };
   // TODO: Revalidate topic page
 }
--- src/components/posts/create-form.tsx
+++ src/components/posts/create-form.tsx
@@ -1,13 +1,20 @@
+"use client";
+
 import {
   Button,
   Input,
   Popover,
   PopoverContent,
   PopoverTrigger,
+  Textarea,
 } from "@nextui-org/react";
 import FormButton from "../common/form-button";
+import { useFormState } from "react-dom";
+import actions from "@/actions";
 
 export default function PostCreateForm() {
+  const [formState, action] = useFormState(actions.createPost, { errors: {} });
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
@@ -15,7 +22,7 @@ export default function PostCreateForm() {
       </PopoverTrigger>
 
       <PopoverContent>
-        <form action="">
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-ld">Create a Post</h3>
 
@@ -24,15 +31,24 @@ export default function PostCreateForm() {
               label="Title"
               labelPlacement="outside"
               placeholder="Title"
+              isInvalid={!!formState.errors.title}
+              errorMessage={formState.errors.title?.join(", ")}
             />
 
-            <Input
+            <Textarea
               name="content"
               label="Content"
               labelPlacement="outside"
               placeholder="Content"
+              isInvalid={!!formState.errors.content}
+              errorMessage={formState.errors.content?.join(", ")}
             />
 
+            {formState.errors._form ? (
+              <div className="rounded p-2 bg-red-200 border border-red-400">
+                {formState.errors._form.join(", ")}
+              </div>
+            ) : null}
             <FormButton>Save</FormButton>
           </div>
         </form>
