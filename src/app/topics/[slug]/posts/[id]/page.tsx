edc087308f6b4155309f72fab2487a640d656fda
Martin Zane <martin@zaneweb.com>
2024-05-10 09:32:40 +0200

    Add skeleton to post show

--- src/app/topics/[slug]/posts/[id]/page.tsx
+++ src/app/topics/[slug]/posts/[id]/page.tsx
@@ -4,6 +4,8 @@ import PostShow from "@/components/posts/show";
 import CommentCreateForm from "@/components/comments/create-form";
 import CommentList from "@/components/comments/list";
 import { fetchCommentsByPostId } from "@/db/queries/comments";
+import { Suspense } from "react";
+import PostShowSkeleton from "@/components/comments/show-skeleton";
 
 interface PostShowPageProps {
   params: {
@@ -20,7 +22,9 @@ export default async function PostPage({ params }: PostShowPageProps) {
       <Link className="underline decoration-solid" href={paths.topic(slug)}>
         {"< "}Back to {slug}
       </Link>
-      <PostShow id={id} />
+      <Suspense fallback={<PostShowSkeleton />}>
+        <PostShow id={id} />
+      </Suspense>
       <CommentCreateForm postId={id} />
       <CommentList postId={id} />
     </div>
